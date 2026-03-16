e406d0a2b3266dfbb10e361fe392e36701f2c90e
jaseena-k <jaseenajas596@gmail.com>
2026-03-16 12:36:27 +0530

    project started

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from ('express')
+import productControls from ("./controls/product_controls")
+import productRoutes from ("./routes/product_routes")
+
+
+
+const app = express()
+
+app.listen()
